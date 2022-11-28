14fd2fccaf3b50599301d946518f2ca551ab68d6
JOAO VICTOR QUEIROZ  VICTOR <109996770+joaoqueirozyerebit@users.noreply.github.com>
2022-11-28 00:52:37 +0000

    binary tree final_version

--- binary_tree.py
+++ binary_tree.py
@@ -19,42 +19,51 @@ class BinaryTree:
         # adubo
         anterior = root
         # crescer para direita
-        if value < root.value:
+        if value < root:
           root = root.direita
           if root == None:
             anterior.direita = node
           return
       else:
         # crescer para esquerda 
-        if value > root.value:
+        if value > root:
           root = root.esquerda
           if root == None:
             anterior.esquerda = node
           return
 
-  def remover(self, value):
-    root = self.root
-    # prucurar
-    while root.value != value:
-      if value < root.value:
-        root = root.esquerda
-      return
-      if value > root.value:
-        root = root.direita
-      return
-    # remover
-    if root.value == value:
-      root.volue == None
-
-  def percorer(self, value):
-    root = self.root
-    while root.value != value:
-      if value < root.value:
-        root = root.esquerda
-      return
-      if value > root.value:
-        root = root.direita
-      return
+  def _remove(self, value, node):
+    if value < node.value:
+      node.left = self._remove(value, node.left)
+    elif value > node.value:
+      node.right = self._remove(value, node.right)
+    else:
+      if node.left is None:
+        return node.right
+      elif node.right is None:
+        return node.left
+      else:
+        def _menorFolha(node=None):
+          while node.left:
+            node = node.left
+          return node.value
+        organize = _menorFolha(node.right)
+        node.data = organize
+        node.right = self._remove(organize, node.right)
+    return node
+  
+  def buscar(self, value):
+    if self.root == None:
+      return None
+    atual = self.root # começa a procurar desde raiz
+    while atual.item != value: # enquanto nao encontrou
+      if value < atual.item:
+        atual = atual.esq # caminha para esquerda
+      else:
+        atual = atual.dir # caminha para direita
+        if atual == None:
+          return None # encontrou uma folha -> sai
+      return atual  # terminou o laço while e chegou aqui é pq encontrou item   
 # posordem
   def imprimir(self,root):
     self.root = root
